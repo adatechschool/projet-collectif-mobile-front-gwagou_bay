a9e15ef45c89f736dc42fa576d351d9c389b5a36
elinelgp <elinelgp@gmail.com>
2024-01-11 16:15:18 +0100

    création navbar

--- app/src/main/java/com/example/gwagou_bay/MainActivity.kt
+++ app/src/main/java/com/example/gwagou_bay/MainActivity.kt
@@ -9,10 +9,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.gwagou_bay.databinding.ActivityMainBinding
 import com.example.gwagou_bay.fragments.AddSpotFragment
 import com.example.gwagou_bay.fragments.HomeFragment
 import com.example.gwagou_bay.fragments.SpotDetailsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 //private lateinit var appBarConfiguration: AppBarConfiguration
@@ -21,13 +25,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(HomeFragment())
+
 //        binding = ActivityMainBinding.inflate(layoutInflater)
+        //import de la navbar
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+        navigationView.setOnItemSelectedListener{
+            when(it.itemId)
+            {
+                R.id.add_spot_page -> {
+                    loadFragment(AddSpotFragment(this))
+                    return@setOnItemSelectedListener true
+                }
+                R.id.settings_page -> {
+                    loadFragment(SpotDetailsFragment(this))
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> false
+            }
+        }
+
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+
+
 
         // injecter le fragment dans notre boîte (fragment_container)
         val transaction = supportFragmentManager.beginTransaction() // permet de stocker une valeur (qui ne changera pas) ; support fragment manager (permet de gérer les fragments sur android) ; .begintransaction => commence série d'opération pour gérer les fragments
-        transaction.replace(R.id.fragment_container, AddSpotFragment(this)) // ici on remplace l'élément de gauche, par celui de droite (ici HomeFragment())
+        transaction.replace(R.id.fragment_container, fragment) // ici on remplace l'élément de gauche, par celui de droite (ici HomeFragment())
         transaction.addToBackStack(null) // permet de ne pas avoir de retour sur ce composant
         transaction.commit() // envoie les changements
+
     }
 
 
